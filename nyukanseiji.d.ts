6833e378913d879d41fdf13146fef473a78dfc44
Ryusei Yamaguchi <mandel59@gmail.com>
2022-07-02 13:40:38 +0900

    Add typings file

---
+++ nyukanseiji.d.ts
@@ -0,0 +1,8 @@
+type NyukanSeijiRecord = [string, string] | [string, string, string];
+
+declare const NyukanSeiji: {
+  table4_1: NyukanSeijiRecord[];
+  table4_2: NyukanSeijiRecord[];
+};
+
+export = NyukanSeiji;
